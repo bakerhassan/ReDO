2c081e820dc63ea189bcd890aebaf7e6068010fb
bakerhassan <61099003+bakerhassan@users.noreply.github.com>
2025-06-08 11:47:57 -0400

    Update example_load_pretrained.py

--- example_load_pretrained.py
+++ example_load_pretrained.py
@@ -167,16 +167,16 @@ with torch.no_grad():
     mData_rgb = mData.expand(-1, 3, -1, -1)  # [N, 1, H, W] → [N, 3, H, W]
     mPred0_rgb = mPred[:, 0:1].expand(-1, 3, -1, -1)
     mPred1_rgb = (mPred[:, 1:2] >= 0.5).float().expand(-1, 3, -1, -1)
-    print(mPred.shape)
     # Concatenate all
     out = torch.cat((
         xData_color.cpu(),  # colored
         mData_rgb.cpu(),  # true masks
         mPred0_rgb.cpu(),  # grayscale/binary
         mPred1_rgb.cpu(),  # grayscale/binary thresholded
+        (mPred0_rgb + mPred1_rgb).cpu(),
         xGen0_color.cpu(),  # colored
         xGen1_color.cpu()  # colored
     ), dim=1)
 
 
-    torchvision.utils.save_image(out.view(-1,3,128,128), 'out.png', normalize=True, nrow=6)
+    torchvision.utils.save_image(out.view(-1,3,128,128), 'out.png', normalize=True, nrow=7)
