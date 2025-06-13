dc5309aa4f432b8935b83620e4a674a18b69d842
bakerhassan <61099003+bakerhassan@users.noreply.github.com>
2025-06-13 18:21:17 -0400

    Update train.py

--- train.py
+++ train.py
@@ -361,7 +361,7 @@ while opt.iteration <= opt.nIteration:
         hGen = netGenX(mEnc, zData)
         xGen = (hGen + ((1 - mEnc.unsqueeze(2)) * xData.unsqueeze(1))).view(hGen.size(0) * hGen.size(1), hGen.size(2), hGen.size(3), hGen.size(4))
         dGen = netDX(xGen)
-        lossG += - dGen.mean()
+        lossG = lossG - dGen.mean()
         if opt.wrecZ > 0:
             zRec = netRecZ(hGen.sum(1))
             err_recZ = ((zData - zRec) * (zData - zRec)).mean()
